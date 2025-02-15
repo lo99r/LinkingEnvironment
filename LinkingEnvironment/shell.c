5eb7e5e71c40df3cedfcbaac97ac3035beeb2dca
00brng@honeys.be <>
2025-02-15 22:12:19 +0900

    '6.0.4'

--- LinkingEnvironment/shell.c
+++ LinkingEnvironment/shell.c
@@ -14,15 +14,28 @@ int Funsion_StartShell() {
 }
 
 int Funsion_shell() {
+	static char* babad_gugas;
+	//inp = input;
 	while (1) {
-		scanf("%[^\n]", inpur);
-		sscanf("%s", input[0]);
+		printf("_\n");
+		getchar();
+		scanf_s("%[^\n]", inpur, sizeof(inpur));
+		babad_gugas = strtok(inpur, " ");
+		strcpy(input[0], inpur);
+		//sscanf_s(inpur, "%s", input[0], sizeof(input[0]));
 		if (strcmp(input[0], "test") == 0) {
 			printf("print\n");
 		}
 		else if (strcmp(input[0], "ver") == 0) {
 			//full_versions(le_ver, sub_ver, build_ver);
 			printf("LinkingEncironment version %d.%d.%d", le_ver, sub_ver, build_ver);
+		}
+		else if (strcmp(input[0], "opsiontest") == 0) {
+			//sscanf_s(inpur, "%s", input[1], sizeof(input[1]));
+			strcpy(input[1], babad_gugas);
+			if (strcmp(input[1], "opsiontest") == 0) {
+				printf("t");
+			}
 		}
 	}
 }

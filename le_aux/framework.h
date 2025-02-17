61bf2e89060c0a6932ff91f7f295a200b8d0a373
00brng@honeys.be <>
2025-02-17 17:10:52 +0900

    1.0.0.a.P003.A10000H06-1

--- le_aux/framework.h
+++ le_aux/framework.h
@@ -3,3 +3,23 @@
 #define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
 // Windows 헤더 파일
 #include <windows.h>
+
+#include<stdio.h>//include "stdio.h"
+#include<stdlib.h>
+#include<conio.h>//include "conio.h"
+//#include<stdlib.h>//include "stdlib.h"
+#include<math.h>//include "math.h"
+#include<string.h>//include "string.h"
+//#include<windows.h>//include "windows.h"
+#include<process.h>
+#include<io.h>
+
+#define major_LEscript 1
+#define minor_LEscript 0
+#define patch_LEscript 0
+#define abcde_LEscript 1
+#define abcdeNo_LEscript 1
+#define programNo_LEscript 3
+#define nextUpdate_LEscript 65536
+#define LEversion_LEscript 6
+#define build_LEscript 1
--- le_aux/pch.cpp
+++ le_aux/pch.cpp
@@ -3,6 +3,7 @@
 #include "pch.h"
 
 // 미리 컴파일된 헤더를 사용하는 경우 컴파일이 성공하려면 이 소스 파일이 필요합니다.
-extern "C" __declspec(dllexport) int AUX() {
+extern "C" __declspec(dllexport) int LEscript() {
 	//wanahen zuaknauii at kiwold program anas
+	printf("test\n");
 }

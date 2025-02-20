99b08c9906672858a888c710a6b6e54123183821
00brng@honeys.be <>
2025-02-20 19:20:27 +0900

    0.0.0.a.P002.A00000H06-1

--- LEPlud/framework.h
+++ LEPlud/framework.h
@@ -3,3 +3,18 @@
 #define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
 // Windows 헤더 파일
 #include <windows.h>
+#include <stdio.h>
+#include <string.h>
+
+//Program Shell And User Help Kit for AuX
+#define command_M 0
+#define command_N 0
+#define command_P 0
+#define command_AlphabetInfo 1
+#define command_AlphabetP 1
+#define command_ProgramNumber 2
+#define command_A 0
+#define command_H 6
+#define command_B 1
+#define LEN 0
+#define LEP 6
--- LEPlud/pch.cpp
+++ LEPlud/pch.cpp
@@ -1,9 +1,23 @@
 ﻿// pch.cpp: 미리 컴파일된 헤더에 해당하는 소스 파일
 
 #include "pch.h"
+char verfuurr[260] = { 0, };
 
 // 미리 컴파일된 헤더를 사용하는 경우 컴파일이 성공하려면 이 소스 파일이 필요합니다.
 
-extern "C" __declspec(dllexport) int Command_Environment_System() {
-	printf();
+extern "C" __declspec(dllexport) int Command_Environment_System(char Command_Prompt[260]) {
+	//printf();
+	//switch
+	if (strcmp(Command_Prompt, "ver")) {
+		sprintf(verfuurr, "%d.%d.%d.%c%d.P%03d.A%05XH%02d-%d", command_M, command_N, command_P, command_AlphabetInfo - 1 + 'a', command_AlphabetP, command_ProgramNumber, command_A, command_H, command_B); \
+			//
+			return (int)verfuurr;
+	}
+}
+
+extern "C" __declspec(dllexport) char Command_ErrorCode(int number) {
+	switch (number) {
+	case 2002:
+		sprintf(verfuurr, "Print: mardalaa at anas");
+	}
 }
